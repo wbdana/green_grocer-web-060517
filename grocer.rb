ed44fcca5b23f047aefe0816dd1bee4f2b506d23
William Dana <william.b.dana@gmail.com>
2017-05-25 00:33:13 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,53 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  consolidated_cart = {}
+  item_names = []
+  cart.each do |item_with_info|
+    item_with_info.each do |k, v|
+      consolidated_cart[k] = v
+      if !item_names.include?(k)
+        item_names << k
+        consolidated_cart[k][:count] = 1
+      elsif item_names.include?(k)
+        consolidated_cart[k][:count] += 1
+        item_names.include?(k)
+      end
+    end
+  end
+  consolidated_cart
 end
 
+cart = {
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 3},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1}
+}
+
+coupons = {:item => "AVOCADO", :num => 2, :cost => 5.0}
+
+goal = {
+  "AVOCADO" => {:price => 3.0, :clearance => true, :count => 1},
+  "KALE"    => {:price => 3.0, :clearance => false, :count => 1},
+  "AVOCADO W/COUPON" => {:price => 5.0, :clearance => true, :count => 1},
+}
+
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |k, v|
+    if cart.keys.include?(coupons[:item])
+      cart["#{coupons[:item]} W/COUPON"] = {}
+      cart["#{coupons[:item]} W/COUPON"][:price] = coupons[:cost]
+      cart["#{coupons[:item]} W/COUPON"][:clearance] = cart[coupons[:item]][:clearance]
+      cart["#{coupons[:item]} W/COUPON"][:count] = cart[coupons[:item]][:count] % coupons[:num]
+      cart[coupons[:item]][:count] = cart[coupons[:item]][:count] % coupons[:num]
+    end
+  end
+  return items
 end
 
 def apply_clearance(cart)
-  # code here
+
 end
 
 def checkout(cart, coupons)
-  # code here
+
 end
